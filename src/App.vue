91d83b78720f1139ad7b56fe799a42573f367b3d
wal0x <h.medwail@gmail.com>
2022-04-18 11:46:14 +0200

    Refactor Icons

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import {
   LANGUAGES,
   vueI18n,
 } from "./i18n/i18n.helper";
+import IconBurger from "./components/icons/IconBurger.vue";
 let showMenu = ref(false);
 
 const currentLanguage = computed(() => vueI18n.global.locale);
@@ -43,12 +44,7 @@ onMounted(() => {
               type="button"
               class="text-gray-100 hover:text-blue-400 active:text-blue-400 focus:outline-0 focus:bg-transparent focus:text-gray-400"
             >
-              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-                <path
-                  fill-rule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                ></path>
-              </svg>
+              <icon-burger />
             </button>
           </div>
         </div>
---
+++ src/components/icons/IconBurger.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+    <path
+      fill-rule="evenodd"
+      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+    ></path>
+  </svg>
+</template>
